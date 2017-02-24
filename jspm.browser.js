1de3c4df0f11a86d0f9c936829e1758490d69d39
Jason Pearson <jasonp.pdx@gmail.com>
2017-02-24 12:58:53 -0500

    switch from jspm/gulp to webpack (#13)

--- jspm.browser.js
+++
@@ -1,9 +0,0 @@
-SystemJS.config({
-  baseURL: "/",
-  paths: {
-    "src/react-uploader": "react-uploader",
-    "github:": "jspm_packages/github/",
-    "npm:": "jspm_packages/npm/",
-    "react-uploader/": "src/"
-  }
-});
